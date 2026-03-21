5ea11cf5703511bafe5ef1da8610aa3799378a02
santosverseia-tech <santosverse.ia@gmail.com>
2026-03-21 13:14:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,3 +19,10 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // vamos ajustar isso abaixo 👇
+})
